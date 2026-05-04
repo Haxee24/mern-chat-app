b223925ca5dabc4a80e67b37f4cc35915c62e97f
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-05-04 14:12:50 +0530

    created ApiError class utility for convenience

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,19 @@
+export default class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="Something went wrong",
+        error=[],
+        stack=""
+    ) {
+        super(message);
+        this.data = null;
+        this.statusCode = statusCode;
+        this.success = false;
+        this.error = error;
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
