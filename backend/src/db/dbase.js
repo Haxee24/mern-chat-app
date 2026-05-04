793d1285641604d812e81120cf7c05ee25e1b079
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-05-04 14:28:24 +0530

    used connectionInstance in db file

--- backend/src/db/dbase.js
+++ backend/src/db/dbase.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 export default async function connectDB() {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`);
-        console.log("Connected to MONGODB");
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`);
+        console.log("Connected to MONGODB: " + connectionInstance.connection.host);
     } catch (error) {
         console.error("Error connecting to MONGODB", error);
         process.exit(1);
